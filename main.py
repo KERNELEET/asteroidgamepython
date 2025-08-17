921d91c18e8dda9de55e3fa69f8d53a6cf4785ba
KERNELEET <ahmed.kib3@gmail.com>
2025-08-17 19:05:28 +0500

    Added A Black Responsive Screen

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+from constants import *
+import os
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill("black")
+        pygame.display.flip()
+    pygame.quit()
+if __name__ == "__main__":
+    main()
