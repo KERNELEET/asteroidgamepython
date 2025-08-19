ec96db12c46bc74ca48505ef4a260bf18c862625
KERNELEET <ahmed.kib3@gmail.com>
2025-08-19 18:30:51 +0500

    Asteroids Updated

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pygame
+from circleshape import CircleShape
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 import os
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
 
@@ -9,13 +14,45 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    background = pygame.image.load("background.jpg")
+    background = pygame.transform.scale(background,(SCREEN_WIDTH,SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
+    Asteroid.containers = (updatable,drawable,asteroids)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (updatable,drawable,shots)
+    pygame.display.set_caption("Asteroids")
+    asteroid_field = AsteroidField()
     running = True
+    clock = pygame.time.Clock()
+    dt = 0
+    x =  SCREEN_WIDTH/ 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x,y)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
-        screen.fill("black")
+                return
+        screen.blit(background,(0,0))
+        for d in drawable:
+            d.draw(screen)
+        for u in updatable:
+            u.update(dt)
+        asteroids:Asteroid
+        for asteroid in asteroids:
+            if asteroid.has_collided_with(player):
+                print("Game Over!")
+                return
+        shot: Shot
+        for shot in shots:
+            for asteroid in asteroids:
+             if asteroid.has_collided_with(shot):
+                shot.kill()
+                asteroid.split()
         pygame.display.flip()
-    pygame.quit()
+        dt = clock.tick(60) / 1000
 if __name__ == "__main__":
     main()
